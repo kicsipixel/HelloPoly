b388eff383ceca2fb3d978531fd7a13cfa9e6587
Szabolcs Toth <tsz@purzelbaum.hu>
2019-11-30 15:47:43 +0100

    🏁 Fixed filled or stroke, now it seems to be final.

--- HelloPoly/ContentView.swift
+++ HelloPoly/ContentView.swift
@@ -15,44 +15,52 @@ struct ContentView: View {
     
     var arrayOfPoint = [Float]()
     
+    struct Poly: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            path.move(
+                    to: CGPoint(
+                        x: rect.size.width / 2  + PolygonParameters.points[0].xFactor,
+                        y: rect.size.height / 2 + PolygonParameters.points[0].yFactor
+                    )
+                )
+                
+                PolygonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: rect.size.width / 2 + $0.xFactor,
+                            y: rect.size.height / 2 + $0.yFactor
+                        )
+                    )
+                }
+                path.closeSubpath()
+            return path
+        }
+    }
+    
     var body: some View {
         createPoints(numberOfSides: numberOfSides)
+        
         return NavigationView {
             VStack  {
-                Form {
+               VStack {
                     Stepper(value: $numberOfSides, in: 3...12, label: { Text("Number of sides: \(numberOfSides)")})
                     Toggle(isOn: $filledPolygon) { Text("Filled")}
+                    Text("The interior angle is: \(Radians()[0], specifier: "%.0f")° or \(Radians()[1], specifier: "%.4f") rad")
                 }
-                    .frame(maxHeight: 124)
-                     .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
-                    Text("\(Polygons())")
-                        .font(.system(size: 28))
-                        .padding(.bottom, 10)
-                    Text("The sum of interior angles: \(Radians()[0], specifier: "%.0f")° and \(Radians()[1], specifier: "%.0f") rad")
-                    Path { path in
-                        path.move(
-                            to: CGPoint(
-                                x: PolygonParameters.points[0].xFactor,
-                                y: PolygonParameters.points[0].yFactor
-                            )
-                        )
-                        
-                        PolygonParameters.points.forEach {
-                            path.addLine(
-                                to: .init(
-                                    x: $0.xFactor,
-                                    y: $0.yFactor
-                                )
-                            )
-                        }
-                        path.closeSubpath()
-                    }
-                    .fill( LinearGradient(
-                               gradient: Gradient(colors: [.green, .white]),
-                               startPoint: .top,
-                               endPoint: .bottom
-                           ))
-                    .opacity(0.9)
+                .padding()
+               .offset(y: -180)
+                               
+                Text("\(Polygons())")
+                    .font(.system(size: 24))
+                    .foregroundColor(filledPolygon ? .white  : .green)
+                    .background(filledPolygon ?
+                        Poly().fill(Color.green)
+                            .overlay(Poly().stroke(Color.green)) :
+                        Poly().fill(Color.white)
+                            .overlay(Poly().stroke(Color.green, lineWidth: 2))
+                    )
                 }
              .navigationBarTitle(Text("HelloPoly"))
         }
@@ -63,28 +71,28 @@ struct ContentView: View {
         case 3:
            return "Triangle"
         case 4:
-            return "Quadrilateral or Tetragon"
+            return "Quadrilateral"
         case 5:
             return "Pentagon"
         case 6:
             return "Hexagon"
         case 7:
-            return "Heptagon or Septagon"
+            return "Heptagon"
         case 8:
             return "Octagon"
         case 9:
-            return "Nonagon or Enneagon"
+            return "Nonagon"
         case 10:
             return "Decagon"
         case 11:
-            return "Hendecagon or Undecagon"
+            return "Hendecagon"
         default:
-           return "Dodecagon or Duodecagon"
+           return "Dodecagon"
         }
     }
     
     func Radians() -> [Double] {
-        return [Double((numberOfSides-2) * 180), Double((numberOfSides - 2)) * Double.pi]
+        return [Double((numberOfSides - 2) * 180 / numberOfSides), Double(Double(numberOfSides - 2) * Double.pi / Double(numberOfSides))]
     }
     
     func createPoints(numberOfSides: Int) {
@@ -92,8 +100,8 @@ struct ContentView: View {
         for i in 0...numberOfSides-1 {
             PolygonParameters.points.append(
                 PolygonParameters.Segment(
-                    xFactor:  (CGFloat(175 + 100 * sin(Double(i) * 2 * Double.pi / Double(numberOfSides)))),
-                    yFactor:  (CGFloat(150 + 100 * cos(Double(i) * 2 * Double.pi / Double(numberOfSides))))
+                    xFactor:  (CGFloat(120 * sin(Double(i) * 2 * Double.pi / Double(numberOfSides)))),
+                    yFactor:  (CGFloat(120 * cos(Double(i) * 2 * Double.pi / Double(numberOfSides))))
                 )
             )
         }
